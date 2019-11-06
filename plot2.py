e8816e357d36097d599b87404496065fc4663917
RounakChatterjee <56823119+RounakChatterjee@users.noreply.github.com>
2019-11-06 16:24:08 +0530

    Delete plot2.py

--- plot2.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-wavn = np.loadtxt("C:/Users/ROUNAK/Desktop/study/recent assignments/Pending/python codes/NASA_data.txt",dtype = 'float',comments = '#',delimiter = None,usecols = (0))
-Itn = np.loadtxt("C:/Users/ROUNAK/Desktop/study/recent assignments/Pending/python codes/NASA_data.txt",dtype = 'float',comments = '#',delimiter = None,usecols = (1))
-print(Itn)
-print(wavn)
-plt.scatter(wavn , Itn)
-plt.grid()
-plt.show()
